e605b9de297edb778411f1b2eabafa820e9fabe4
SpicyChickenFLY <xixiayuchao@gmail.com>
2022-10-24 16:37:40 +0800

    update auto client

--- auto_client.sh
+++ auto_client.sh
@@ -1,44 +1,9 @@
 #! /bin/sh
 
-# select * from Typecast_table_1;
-# select * from Typecast_table_1 WHERE age>'11';
-
-# select * from Typecast_table_2;
-# select * from Typecast_table_2 WHERE age<=10;
-
-# select * from Typecast_table_3;
-# select * from Typecast_table_3 WHERE id<'1.5a';
-# select * from Typecast_table_3 WHERE id>='3a';
-
 build/bin/obclient << EOF
-SELECT * FROM Typecast_table_4;
-SELECT * FROM Typecast_table_4 WHERE 'a'<1;
-SELECT * FROM Typecast_table_4 WHERE '1a'>0;
-SELECT * FROM Typecast_table_4 WHERE '1a'>0.5;
 
 exit
 EOF
-# CREATE TABLE Typecast_table_4(id int, name char, age float);
-# INSERT INTO Typecast_table_4 VALUES (1,1,1);
-# SELECT * FROM Typecast_table_4;
-# SELECT * FROM Typecast_table_4 WHERE 'a'<1;
-# SELECT * FROM Typecast_table_4 WHERE '1a'>0;
-# SELECT * FROM Typecast_table_4 WHERE '1a'>0.5;
-
-# CREATE TABLE Typecast_table_3(id int, name char, age float);
-# INSERT INTO Typecast_table_3 VALUES (1,1,1);
-# INSERT INTO Typecast_table_3 VALUES (2,2,2);
-# INSERT INTO Typecast_table_3 VALUES (3,3,3);
-# select * from Typecast_table_3;
-# select * from Typecast_table_3 WHERE id<'1.5a';
-# select * from Typecast_table_3 WHERE id>='3a';
-
-# CREATE TABLE Typecast_table_2(id int, name char, age float);
-# INSERT INTO Typecast_table_2 VALUES (0.4,1.5,9.5);
-# INSERT INTO Typecast_table_2 VALUES (1.0,2.50,9.5);
-# INSERT INTO Typecast_table_2 VALUES (1.9,'3.5',11.5);
-# select * from Typecast_table_2;
-# select * from Typecast_table_2 WHERE age<=10;
 
 # CREATE TABLE Typecast_table_1(id int, name char, age float);
 # INSERT INTO Typecast_table_1 VALUES ('cc','c','ccc');
@@ -47,6 +12,25 @@ EOF
 # INSERT INTO Typecast_table_1 VALUES ('2','b','11.5b');
 # select * from Typecast_table_1;
 # select * from Typecast_table_1 WHERE age>'11';
+# CREATE TABLE Typecast_table_2(id int, name char, age float);
+# INSERT INTO Typecast_table_2 VALUES (0.4,1.5,9.5);
+# INSERT INTO Typecast_table_2 VALUES (1.0,2.50,9.5);
+# INSERT INTO Typecast_table_2 VALUES (1.9,'3.5',11.5);
+# select * from Typecast_table_2;
+# select * from Typecast_table_2 WHERE age<=10;
+# CREATE TABLE Typecast_table_3(id int, name char, age float);
+# INSERT INTO Typecast_table_3 VALUES (1,1,1);
+# INSERT INTO Typecast_table_3 VALUES (2,2,2);
+# INSERT INTO Typecast_table_3 VALUES (3,3,3);
+# select * from Typecast_table_3;
+# select * from Typecast_table_3 WHERE id<'1.5a';
+# select * from Typecast_table_3 WHERE id>='3a';
+# CREATE TABLE Typecast_table_4(id int, name char, age float);
+# INSERT INTO Typecast_table_4 VALUES (1,1,1);
+# SELECT * FROM Typecast_table_4;
+# SELECT * FROM Typecast_table_4 WHERE 'a'<1;
+# SELECT * FROM Typecast_table_4 WHERE '1a'>0;
+# SELECT * FROM Typecast_table_4 WHERE '1a'>0.5;
 
 
 # CREATE TABLE Select_tables_1(id int, age int, u_name char);
